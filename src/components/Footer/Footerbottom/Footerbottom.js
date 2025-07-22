3e5b47858c2f7b46f4be0115c80df9a9952fe914
tduncan14 <nbatre1@yahoo.com>
2025-07-21 22:19:51 -0500

    keep moving forward

--- src/components/Footer/Footerbottom/Footerbottom.js
+++ src/components/Footer/Footerbottom/Footerbottom.js
@@ -4,7 +4,27 @@ import './Footerbottom.css'
 const Footerbottom = () => {
     return (
         <div className="footerbottom">
-            bottom
+            <div className="leftFoot">
+
+                <p><span></span>Nintendo. Games are property of their respective owners. Nintendo of America Inc. Headquarters are in Redmond, Washington, USA</p>
+            </div>
+
+
+            <div className="rightFoot">
+                <ul>
+                    <li>Contact</li>
+                    <li> Website feedback</li>
+                    <li> Term Of Use</li>
+                    <li>Documents & Policies</li>
+                </ul>
+
+                <div className="flag">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg" alt="US Flag" />
+
+
+                    <h3>Change region</h3>
+                </div>
+            </div>
         </div>
     )
 }
